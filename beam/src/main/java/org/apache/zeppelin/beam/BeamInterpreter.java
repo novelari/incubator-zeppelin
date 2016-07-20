f87657e6167e56f75c05575ba5dcc2acdfadbdfc
mahmoudelgamal <mahmoudf.elgamal@gmail.com>
2016-07-20 21:32:24 +0200

    Removing some classes

--- beam/src/main/java/org/apache/zeppelin/beam/BeamInterpreter.java
+++ beam/src/main/java/org/apache/zeppelin/beam/BeamInterpreter.java
@@ -34,31 +34,10 @@ public class BeamInterpreter extends Interpreter {
         new InterpreterPropertyBuilder().build());
   }
 
-  public static void main(String[] args) {
-    String base = "src/main/java/org/apache/beam/examples";
-    String class1name = MinimalWordCount.class.getCanonicalName();
-    String class1Code = FileUtil.readUTF8(base + "/MinimalWordCount.java");
-    InterpreterResult x = new BeamInterpreter(null).interpret(class1Code, null);
-    System.out.println(x.message());
-  }
 
   @Override
   public void open() {
-    // ConsoleConfig config = consoleConfig()
-    // .historyFile(new File(getProperty("user.home"), ".javarepl-embedded.history"))
-    // .commands(ListValues.class, ShowHistory.class, EvaluateFromHistory.class,
-    // SearchHistory.class).results(result("date", new Date()), result("num", 42));
-    //
-    // try {
-    // new RestConsole(new SimpleConsole(config), 8001);
-    // } catch (Exception e) {
-    // e.printStackTrace();
-    // }
-    // try {
-    // replServer = new ReplServer("localhost", 5555);
-    // } catch (Exception e) {
-    // e.printStackTrace();
-    // }
+
 
   }
 
@@ -67,53 +46,12 @@ public class BeamInterpreter extends Interpreter {
 
   }
 
-  // @Override
-  // public InterpreterResult interpret(String st, InterpreterContext context) {
-  //
-  // List<Map<String, String>> logs = replServer.execute(st);
-  // String successfullyMsg = "";
-  // for (int i = 0; i < logs.size(); i++) {
-  // if (logs.get(i).get("type").equals("ERROR")) {
-  // return new InterpreterResult(InterpreterResult.Code.ERROR, logs.get(i).get("message"));
-  // } else if (logs.get(i).get("type").equals("SUCCESS") &&
-  // !logs.get(i).get("message").isEmpty()) {
-  // successfullyMsg += logs.get(i).get("message") + "\n";
-  // }
-  //
-  // }
-  // return new InterpreterResult(InterpreterResult.Code.SUCCESS, successfullyMsg);
-  //
-  // }
   @Override
   public InterpreterResult interpret(String st, InterpreterContext context) {
-    // System.out.println(st);
     String uuid = "C" + UUID.randomUUID().toString().replace("-", "");
-    // System.out.println(uuid);
-    // StringWriter writer = new StringWriter();
-    // PrintWriter out = new PrintWriter(writer);
-    //
-    // out.println("import org.apache.beam.runners.dataflow.BlockingDataflowPipelineRunner;\n"
-    // + "import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;\n"
-    // + "import org.apache.beam.sdk.Pipeline;\n" + "import org.apache.beam.sdk.io.TextIO;\n"
-    // + "import org.apache.beam.sdk.options.PipelineOptionsFactory;\n"
-    // + "import org.apache.beam.sdk.transforms.Count;\n"
-    // + "import org.apache.beam.sdk.transforms.DoFn;\n"
-    // + "import org.apache.beam.sdk.transforms.MapElements;\n"
-    // + "import org.apache.beam.sdk.transforms.ParDo;\n"
-    // + "import org.apache.beam.sdk.transforms.SimpleFunction;\n"
-    // + "import org.apache.beam.sdk.values.KV;\n");
-    // out.println("public class " + uuid + " {");
-    // out.println("  public static void main(String args[]) {");
-    // out.println(st);
-    // out.println("  }");
-    // out.println("}");
-    // out.close();
-    //
-    // System.out.println(writer.toString());
-    // Class<?> class1;
+    
     try {
-      // class1 = Compiler.compile(uuid, writer.toString());
-      // Compiler.invokeMain(class1);
+
       String msg = CompileSourceInMemory.execute(uuid, st);
       return new InterpreterResult(InterpreterResult.Code.SUCCESS, msg);
     } catch (Exception e) {
--- beam/src/main/java/org/apache/zeppelin/beam/CompileSourceInMemory.java
+++ beam/src/main/java/org/apache/zeppelin/beam/CompileSourceInMemory.java
@@ -41,11 +41,7 @@ public class CompileSourceInMemory {
     JavaProjectBuilder builder = new JavaProjectBuilder();
     JavaSource src = builder.addSource(new StringReader(code));
 
-    List<String> imports = src.getImports();
-    String importsString = "";
-    for (int i = 0; i < imports.size(); i++) {
-      importsString += "import " + imports.get(i) + ";\n";
-    }
+
     List<JavaClass> classes = src.getClasses();
     String classesSt = "";
     String classMain = "", classMainName = "";
@@ -65,22 +61,12 @@ public class CompileSourceInMemory {
 
     }
     code = code.replace(classMainName, className);
-    // classMain = classMain.replace(classMainName, className);
-    // classesSt = classesSt.replace(classMainName, className);
-    // className = classMainName;
+
     StringWriter writer = new StringWriter();
     PrintWriter out = new PrintWriter(writer);
 
-    out.println("" 
-        // + importsString
-        // + classesSt
-        // + classMain
-        + code);
-    // out.println("public class " + className + " {");
-    // out.println("  public static void main(String args[]) {");
-    // out.println(code);
-    // out.println("  }");
-    // out.println("}");
+    out.println(code);
+
     out.close();
 
     System.out.println(writer.toString());
@@ -92,15 +78,13 @@ public class CompileSourceInMemory {
     ByteArrayOutputStream baosErr = new ByteArrayOutputStream();
     PrintStream newOut = new PrintStream(baosOut);
     PrintStream newErr = new PrintStream(baosErr);
+  
     // IMPORTANT: Save the old System.out!
     PrintStream oldOut = System.out;
     PrintStream oldErr = System.err;
     // Tell Java to use your special stream
-//    System.setErr(new PrintStream(new LoggingOutputStream(Category.getRoot(),
-//        Priority.WARN), true));
-
     System.setOut(newOut);
-//    System.setErr(newErr);
+    System.setErr(newErr);
 
 
     CompilationTask task = compiler.getTask(null, null, diagnostics, null, null, compilationUnits);
@@ -108,23 +92,14 @@ public class CompileSourceInMemory {
     boolean success = task.call();
     if (!success) {
       for (Diagnostic diagnostic : diagnostics.getDiagnostics()) {
-        // System.out.println(diagnostic.getCode());
-        // System.out.println(diagnostic.getKind());
-        // System.out.println(diagnostic.getPosition());
-        // System.out.println(diagnostic.getStartPosition());
-        // System.out.println(diagnostic.getEndPosition());
-        // System.out.println(diagnostic.getSource());
         System.out.println(diagnostic.getMessage(null));
 
       }
     }
-    // System.out.println("Success: " + success);
 
     if (success) {
       try {
-        // Create a stream to hold the output
 
-        // Put things back
 
         URLClassLoader classLoader = URLClassLoader.newInstance(new URL[] { new File("").toURI()
             .toURL() });
@@ -134,10 +109,11 @@ public class CompileSourceInMemory {
 
         System.out.flush();
         System.err.flush();
+        
         System.setOut(oldOut);
         System.setErr(oldErr);
-        // // Show what happened
-        // System.out.println("Here: " + baos.toString());
+       
+        
         classLoader.clearAssertionStatus();
 
         return baosOut.toString();
